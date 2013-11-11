7afd7ec0f62948e35a08c411fa6f12c6067c9eec
Michael Slate <slatem@gmail.com>
2013-11-10 20:01:12 -0500

    initial commit for Ruby Blog example using HAML

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,35 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+  def new
+    @post = Post.new
+  end
+  def create
+    @post = Post.new(post_params)
+   if @post.save
+    redirect_to @post
+   else
+     render 'new'
+   end
+  end
+  def show
+    @post = Post.find(params[:id])
+  end
+  def edit
+    @post = Post.find(params[:id])
+  end
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(params[:post].permit(:title,:text))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+  private
+    def post_params
+        params.require(:post).permit(:title,:text)
+    end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Slambook::Application.config.secret_key_base = 'ea3bf8183648cc8893e41ea005e7629bda7438b0d6108f6881b18d02bcca873029177302357f7ed4f7548e79ee1b6320d67de2b6cacf6c22c771bf3abb02d58d'
